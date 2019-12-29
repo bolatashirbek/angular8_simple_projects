198f9e40c06b2054dd59cc78169e594c6fe0a354
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-12-29 22:37:33 +0600

    ng-basics: added card component

--- ng-basics/src/app/card/card.component.ts
+++ ng-basics/src/app/card/card.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  templateUrl: './card.component.html'
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
 
