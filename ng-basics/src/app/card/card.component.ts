374e8a781be28a60763c645692e2b58cee9d6c0b
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-12-31 18:43:37 +0600

    ng-basics: component dynamic binding

--- ng-basics/src/app/card/card.component.ts
+++ ng-basics/src/app/card/card.component.ts
@@ -1,15 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   title = 'My Card Title';
   text = 'My sample text!';
 
+  disabled = false;
+
   imgUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.imgUrl = 'https://vuejs.org/images/logo.png';
+      this.disabled = true;
+    }, 3000);
+  }
 
   getInfo(): string {
     return 'This is my info';
