c93d4dceb22509f9bddab9873dd46f739eaa2c3c
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-12-31 23:04:30 +0600

    ng-basics: pipe date with locale

--- ng-basics/src/app/app.module.ts
+++ ng-basics/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {registerLocaleData} from '@angular/common';
+import localeKz from '@angular/common/locales/kk';
 
 import {AppComponent} from './app.component';
 import {CardComponent} from './card/card.component';
 import {FormComponent} from './form/form.component';
+
+registerLocaleData(localeKz, 'kz');
 
 @NgModule({
   declarations: [
